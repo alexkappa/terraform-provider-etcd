a51339055a10ac7408559f4f1227a39dd07d8e11
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2015-11-24 14:49:24 +0100

    Cleanup examples

--- example/etcd.tf
+++ example/etcd.tf
@@ -2,14 +2,6 @@ provider "etcd" {
 	endpoints = ["http://localhost:2379", "http://localhost:4001"]
 }
 
-resource "etcd_discovery" "token" {
-    size = 3
-}
-
-output "etcd_discovery_url" {
-	value = "${etcd_discovery.token.id}"
-}
-
 resource "etcd_key" "foo" {
 	key = "foo"
 	value = "bar"
